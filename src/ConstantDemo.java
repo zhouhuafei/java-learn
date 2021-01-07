5bc86a15089d7de4dc5cfd06a47d1f0452ce5db5
zhouhuafei <1123486116@qq.com>
2021-01-07 23:38:37 +0800

    feat: 🎸 ConstantDemo

---
+++ src/ConstantDemo.java
@@ -0,0 +1,14 @@
+public class ConstantDemo {
+    public static void main(String[] args) { // 快捷键：psvm
+        // 字面值常量 - 字符串
+        System.out.println("abc"); // 快捷键：sout
+        // 字面值常量 - 字节
+        System.out.println('a');
+        // 字面值常量 - 整数
+        System.out.println(10);
+        // 字面值常量 - 小数
+        System.out.println(10.02);
+        // 字面值常量 - 布尔值
+        System.out.println(true);
+    }
+}
