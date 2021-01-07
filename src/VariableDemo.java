06b32460f402ff495658cd3e5e77fa4991c80606
zhouhuafei <1123486116@qq.com>
2021-01-07 23:59:35 +0800

    feat: 🎸 VariableDemo

---
+++ src/VariableDemo.java
@@ -0,0 +1,31 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        // 整数型 - 默认int - 定义long要加L（不区分大小写，但是大写更直观）
+        byte b = 2;
+        System.out.println(b);
+
+        short s = 10;
+        System.out.println(s);
+
+        int i = 20;
+        System.out.println(i);
+
+        long l = 10000000000L;
+        System.out.println(l);
+
+        // 浮点型 - 默认double - 定义float要加F（不区分大小写，但是大写更直观）
+        float f = 10.20F;
+        System.out.println(f);
+
+        double d = 10.20;
+        System.out.println(d);
+
+        // 字符型
+        char c = 'c';
+        System.out.println(c);
+
+        // 布尔型
+        boolean bTrue = true;
+        System.out.println(bTrue);
+    }
+}
